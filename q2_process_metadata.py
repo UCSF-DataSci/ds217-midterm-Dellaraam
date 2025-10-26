be87a86091888a35ff9cc4b81b5e1be4af381855
Dellaraam <136400422+Dellaraam@users.noreply.github.com>
2025-10-26 15:20:44 -0700

    part2

--- q2_process_metadata.py
+++ q2_process_metadata.py
@@ -1,9 +1,11 @@
-# TODO: Add shebang line: #!/usr/bin/env python3
+# TODO: Add shebang line: 
+#!/usr/bin/env python3
+import random
 # Assignment 5, Question 2: Python Data Processing
 # Process configuration files for data generation.
 
 
-def parse_config(filepath: str) -> dict:
+def parse_config(file) -> dict:
     """
     Parse config file (key=value format) into dictionary.
 
@@ -19,8 +21,14 @@ def parse_config(filepath: str) -> dict:
         '100'
     """
     # TODO: Read file, split on '=', create dict
+    with open(file) as file:
+        lines = file.readlines()
+        configs = {}
+        for line in lines: 
+            l = line.strip().split('=')
+            configs[l[0]] = int(l[1])
+        return configs
     pass
-
 
 def validate_config(config: dict) -> dict:
     """
@@ -44,6 +52,10 @@ def validate_config(config: dict) -> dict:
         True
     """
     # TODO: Implement with if/elif/else
+    if config['sample_data_rows'] > 0: return True
+    elif config['sample_data_min'] >= 1: return True
+    elif config['sample_data_max'] > config['sample_data_min']: return True
+    else: return False 
     pass
 
 
@@ -68,6 +80,9 @@ def generate_sample_data(filename: str, config: dict) -> None:
     """
     # TODO: Parse config values (convert strings to int)
     # TODO: Generate random numbers and save to file
+    with open(filename, 'w') as file:
+        for i in range(config['sample_data_rows']):
+            file.write(f'{random.randint(18, 75)}\n')
     # TODO: Use random module with config-specified range
     pass
 
@@ -87,17 +102,35 @@ def calculate_statistics(data: list) -> dict:
         >>> stats['mean']
         30.0
     """
+    data2 = []
+    with open(data) as file:
+        for line in file:
+            data2.append(int(line))
+    stats = {}
     # TODO: Calculate stats
+    sum = 0
+    mid = len(data)//2
+    for datum in data2:
+        sum += datum
+    stats['mean'] = sum/len(data2)
+    stats['sum'] = sum 
+    if len(data) % 2 != 0:
+        stats['median'] = data2[mid]
+    else:
+        stats['median'] = (data2[mid] + data2[mid-1])//2
+    return stats
     pass
 
 
 if __name__ == '__main__':
     # TODO: Test your functions with sample data
     # Example:
-    # config = parse_config('q2_config.txt')
-    # validation = validate_config(config)
-    # generate_sample_data('data/sample_data.csv', config)
-    # 
+    config = parse_config('q2_config.txt')
+    validation = validate_config(config)
+    generate_sample_data('data/sample_data.csv', config)
     # TODO: Read the generated file and calculate statistics
+    stats = calculate_statistics('data/sample_data.csv')
     # TODO: Save statistics to output/statistics.txt
+    with open('output/statistcs.txt', 'w') as file:
+        file.write(f'{stats}')
     pass
